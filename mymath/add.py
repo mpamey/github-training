b54223d8ce8dfe99bece826511c6eb8a8952e643
Maxim de Mey <maximdemey@Maxims-MacBook-Pro.local>
2023-07-11 14:56:02 +0200

    initial commit

---
+++ mymath/add.py
@@ -0,0 +1,3 @@
+def add(x, y):
+    """add two number together"""
+    return x + y
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mymath",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=["wheel"],
+    version="0.1.0rc1",
+    description="mymath",
+    author="",
+    entry_points={
+    },
+)
