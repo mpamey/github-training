5476be4dc96b1efb2574dd152b9f023c61596b18
Lucas Ulsamer <lucasulsamer@Lucass-MacBook-Pro.local>
2023-11-22 15:18:51 +0100

    add new functions

--- mymath/add.py
+++ mymath/add.py
@@ -1,3 +1,15 @@
 def add(x, y):
     """Add two number together."""
     return x + y
+
+
+def substract(x, y):
+    return x - y
+
+
+def divide(x, y):
+    return x / y
+
+
+def multiply(x, y):
+    return x * y
