b18ba1365e0ecd6ff5f922e7f8428127bee477d2
Anne-Li-AH <anne.li.cnossen@ah.nl>
2024-05-14 10:03:04 +0200

    add documentation

--- mymath/subtract.py
+++ mymath/subtract.py
@@ -1,4 +1,11 @@
 # abc
-def subtract(x, y): 
+def subtract(x: float, y: float) -> float:
+    '''
+    Function to subtract to values
+    Input: 
+        x: Value to subtract from 
+        y: Value to subtract
+    Output:
+        Result value of subtraction
+    '''
     return x - y 
-
